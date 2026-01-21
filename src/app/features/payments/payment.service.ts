dd1b2214e874e5066b02c87d5019ab6794a13941
Jeffer <desarrollo@medicalsoft.ai>
2026-01-21 08:45:40 -0500

    ,

--- src/app/features/payments/payment.service.ts
+++ src/app/features/payments/payment.service.ts
@@ -4,15 +4,15 @@ import { map, Observable, tap } from 'rxjs';
 import { environment } from '../../../environment/environment.local';
 import { PagedResponse } from '../../core/models/apiResponse';
 import { PaymentModel } from '../../core/models/paymentModel';
+import { Endpoints } from '../../core/api/Endpoints';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentService {
-  private apiUrl = environment.apiUrl; // tu backend
-
+  private apiUrl = environment.apiUrl; 
   constructor(private http: HttpClient) {}
 
   getPayments(): Observable<PaymentModel[]> {
-    return this.http.get<PagedResponse<PaymentModel>>(`${this.apiUrl}/payments`).pipe(
+    return this.http.get<PagedResponse<PaymentModel>>(`${this.apiUrl}${Endpoints.payments.root}`).pipe(
       map((res) => res.content ?? [])
     );
   }
